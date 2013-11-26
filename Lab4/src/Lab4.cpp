7a9d27ace823b8444158793c5e09320c667b6ded
Samara-S <samara.seepersad@gmail.com>
2013-11-26 14:19:44 -0500

    Still fixing it

--- Lab4/src/Lab4.cpp
+++ Lab4/src/Lab4.cpp
@@ -11,6 +11,6 @@ using namespace std;
 
 int main() {
 	cout << "Hello World!!!" << endl; // prints Hello World!!!
-	cout << "Making sure it's not a fluke ";
+	cout << "Making sure it's not a fluke";
 	return 0;
 }
