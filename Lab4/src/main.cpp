177c0a7672bc10128f009a550a66979eb69cd0f3
Jen Chan <jychan@ryerson.ca>
2013-12-01 14:35:14 -0500

    I THINK IT WORKS NOW PLEASE

---
+++ Lab4/src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 1, 2013
+ *      Author: jychan
+ */
+
+#include <iostream>
+using namespace std;
+
+// alright i'm still testing this
+
+int main() {
+cout << "please work";
+
+return 0;
+}
