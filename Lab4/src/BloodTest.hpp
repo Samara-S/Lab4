2f91a94098f0c76520ddebdf7073bf0340248159
Samara-S <samara.seepersad@gmail.com>
2013-12-01 23:33:25 -0500

    Functions for BloodCount

--- Lab4/src/BloodTest.hpp
+++ Lab4/src/BloodTest.hpp
@@ -10,11 +10,33 @@ class BloodCount {
 
 	void getRBC ();
 	void getWBC ();
-	void CheckBC (const int pRBC, const int pWBC);
+	void CheckBC ();
 };
 
 //void getRBC(); will get the patients RBC count
+BloodCount::getRBC(){
+	cout << "What is the Red Blood Cell Count?\n";
+	cin >> RBC;
+}
+//void getWBC() will get the patients WBC count
+BloodCount::getWBC(){
+	cout << "What is the White Blood Cell Count?\n";
+	cin >> WBC;
+}
+//void CheckBC() will check if they're in normal ranges
+BloodCount::CheckBC(){
+	if(RBC>=4000000 && RBC<=6000000){
+		cout << "RBC Normal\n";
+	} else {
+		cout << "RBC Abnormal\n";
+	}
 
+	if (WBC>=4500 && WBC<=10000){
+		cout << "WBC Normal";
+	} else {
+		cout << "WBC Abnormal";
+	}
+}
 //IgGMarkers looks for signs that the patient has had certain diseases in the past
 
 class IgGMarkers : public BloodCount {
